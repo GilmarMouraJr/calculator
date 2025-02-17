9cf56a53f55e0ef03885f9955bbfacd9e732faf5
Gilmar Jr <156866354+GilmarMouraJr@users.noreply.github.com>
2025-02-17 09:03:18 -0300

    Add initial file structure and functions

---
+++ script.js
@@ -0,0 +1,15 @@
+const add = new function (a, b) {
+    return a + b;
+}
+
+const subtract = new function (a, b) {
+    return a - b;
+}
+
+const multiply = new function (a, b) {
+    return a * b;
+}
+
+const divide = new function (a, b) {
+    return a / b;
+}
