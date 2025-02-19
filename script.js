a7618accc0d3bba33d5ec32d7733ac7946082ac7
GilmarMouraJr <juzinho-gilmar@hotmail.com>
2025-02-19 20:25:39 -0300

    Add operator functionality

--- script.js
+++ script.js
@@ -16,23 +16,50 @@ function divide(a, b) {
 
 function operate(firstNumber, secondNumber, operator) {
     if (operator === "+") {
-        return add(firstNumber, secondNumber)
+        currentNum = add(firstNumber, secondNumber)
     }
     if (operator === "-") {
-        return subtract(firstNumber, secondNumber)
+        currentNum = subtract(firstNumber, secondNumber)
     }
     if (operator === "x") {
-        return multiply(firstNumber, secondNumber)
+        currentNum = multiply(firstNumber, secondNumber)
     }
     if (operator === "/") {
-        return divide(firstNumber, secondNumber)
+        currentNum = divide(firstNumber, secondNumber)
     }
+    display.textContent = currentNum;
 }
 
 function addToDisplay(){
+    if(operatorSelected){
+        display.textContent = "";
+        operatorSelected = false;
+    }
     let number = this.textContent;
     display.textContent += number;
     currentNum = display.textContent;
+}
+
+function processOperator(){
+    if (!isFirstNum){
+        firstNumber = currentNum;
+        isFirstNum = true;
+        operator = this.textContent;
+        operatorSelected = true;
+    } else {
+        secondNumber = currentNum;
+        operate(firstNumber, secondNumber, operator);
+        firstNumber = currentNum;
+        operator = this.textContent;
+        operatorSelected = true;
+    }
+}
+
+function equals(){
+    if(isFirstNum){
+        secondNumber = currentNum;
+        operate(firstNumber, secondNumber, operator);
+    }
 }
 
 const display = document.getElementById("displayP");
@@ -41,9 +68,19 @@ let currentNum = display.textContent;
 let firstNumber = 0;
 let secondNumber = 0;
 let operator = "";
-
+let isFirstNum = false;
+let operatorSelected = false;
+let isNumSelected = false;
 
 const numBtn = document.querySelectorAll(".num");
 numBtn.forEach((btn) => {
     btn.addEventListener("click", addToDisplay);
-})
+});
+
+const opBtn = document.querySelectorAll(".op");
+opBtn.forEach((btn) => {
+    btn.addEventListener("click", processOperator);
+});
+
+const equalsBtn = document.getElementById("equals");
+equalsBtn.addEventListener("click", equals);
